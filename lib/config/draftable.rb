8f86d787ea8e9e96921dbb56b31eb7575817ceb6
Björn Wolf <bjoern@we5.de>
2009-07-27 13:38:01 +0200

    Exclude state column from create/update column config

--- lib/config/draftable.rb
+++ lib/config/draftable.rb
@@ -1,5 +1,8 @@
 module ActiveScaffold::Config
   class Draftable < Base
-    def initialize(core_config); end
+    def initialize(core_config)
+      core_config.create.columns.exclude :state
+      core_config.update.columns.exclude :state
+    end
   end
 end
